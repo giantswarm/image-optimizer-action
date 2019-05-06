35b5715098fce57c1050de6978d4522e149b48c5
Marian Steinbach <marian@giantswarm.io>
2019-05-06 23:00:54 +0200

    Re-enable curl output

--- lib.sh
+++ lib.sh
@@ -26,30 +26,30 @@ _remote_commit() {
 	# shellcheck disable=SC2034  # Unused variables left for readability
 	while read -r _src_mode dst_mode _src_sha dst_sha flag path; do
 		file_payload="{\"encoding\": \"base64\", \"content\": \"$(base64 "$path" | tr -d '\n')\"}"
-		file_response=$(curl -s --fail -H "Authorization: token ${GITHUB_TOKEN}" \
+		file_response=$(curl --fail -H "Authorization: token ${GITHUB_TOKEN}" \
 			-d "$file_payload" \
 			"https://api.github.com/repos/${GITHUB_REPOSITORY}/git/blobs")
 		echo "{ \"mode\": \"${dst_mode}\", \"path\": \"${path}\", \"sha\": $(jq '.sha' <<<"$file_response")}" >>"$tmp_file"
 	done < <(git diff-files)
 
-	head_response="$(curl -s --fail -H "Authorization: token ${GITHUB_TOKEN}" \
+	head_response="$(curl --fail -H "Authorization: token ${GITHUB_TOKEN}" \
 		-X GET \
 		"https://api.github.com/repos/${GITHUB_REPOSITORY}/git/${GITHUB_REF}")"
 	head_sha="$(jq '.object.sha' <<<"$head_response")"
 
 	tree_payload="{\"base_tree\": ${head_sha}, \"tree\": $(jq -s '.' "$tmp_file")}"
-	tree_response="$(curl -s --fail -H "Authorization: token ${GITHUB_TOKEN}" \
+	tree_response="$(curl --fail -H "Authorization: token ${GITHUB_TOKEN}" \
 		-d "$tree_payload" \
 		"https://api.github.com/repos/${GITHUB_REPOSITORY}/git/trees")"
 
 	commit_payload="{\"message\": \"${GITHUB_ACTION}: optimize images\", \"tree\": $(jq '.sha' <<<"$tree_response"), \"parents\": [${head_sha}]}"
-	commit_response="$(curl -s --fail -H "Authorization: token ${GITHUB_TOKEN}" \
+	commit_response="$(curl --fail -H "Authorization: token ${GITHUB_TOKEN}" \
 		-d "$commit_payload" \
 		"https://api.github.com/repos/${GITHUB_REPOSITORY}/git/commits")"
 
 	update_branch_payload="{\"sha\": $(jq '.sha' <<<"$commit_response")}"
 	# shellcheck disable=SC2034  # Unused variables left for readability
-	update_branch_response="$(curl -s --fail -H "Authorization: token ${GITHUB_TOKEN}" \
+	update_branch_response="$(curl --fail -H "Authorization: token ${GITHUB_TOKEN}" \
 		-d "$update_branch_payload" \
 		-X PATCH \
 		"https://api.github.com/repos/${GITHUB_REPOSITORY}/git/${GITHUB_REF}")"
